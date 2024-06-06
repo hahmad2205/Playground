bcf11291104c8aa5ed981f144ef02f03a08fd1d3
hassaan <hassaan.ahmad@cogentlabs.co>
2024-06-06 15:56:07 +0500

    Revision fixed except hardcoded indexes

--- weatherMan.py
+++ weatherMan.py
@@ -14,11 +14,20 @@ args = parser.parse_args()
 def read_file(year_files):
     result = []
     temp = []
+    headings = ""
+    h = []
     for i in year_files:
         file = open(args.path + i,"r")
         file_data = file.read()
+        headings = file_data.splitlines()[0]
         temp.append(file_data.splitlines()[1:])
-       
+    
+    heading = dict()
+    h = headings.split(sep = ",")
+    
+    for index, item in enumerate(h):
+        heading[item] = index
+    
     flattened = []
     for sublist in temp:
         for max in sublist:
@@ -27,7 +36,7 @@ def read_file(year_files):
     for item in flattened:
         result.append(item.split(","))
         
-    return result
+    return result,heading
 
 def given_year(year):
     year_files = []
@@ -39,42 +48,40 @@ def given_year(year):
     for file in os.listdir(args.path):
         if (fnmatch.fnmatch(file, '*' + year + '*.txt')):year_files.append(file)
     
-    result = read_file(year_files)
+    result,heading = read_file(year_files)
     
-    return(result)
-    
-    max_temp = int(result[0][1])
-    min_temp = int(result[0][3])
-    max_humid = int(result[0][7])
+    max_temp = int(result[0][heading["Max TemperatureC"]])
+    min_temp = int(result[0][heading["Min TemperatureC"]])
+    max_humid = int(result[0][heading["Max Humidity"]])
     
     for item in result:
-        if(item[1] == ""):
+        if(item[heading["Max TemperatureC"]] == ""):
             continue
             
-        high_temp = int(item[1])
+        high_temp = int(item[heading["Max TemperatureC"]])
         
         if (max_temp < high_temp):
-            max_temp_date = item[0]
+            max_temp_date = item[heading["PKT"]]
             max_temp = high_temp
             
     for item in result:
-        if(item[3] == ""):
+        if(item[heading["Min TemperatureC"]] == ""):
             continue
         
-        low_temp = int(item[3])
+        low_temp = int(item[heading["Min TemperatureC"]])
     
         if min_temp > low_temp:
-            min_temp_date = item[0]
+            min_temp_date = item[heading["PKT"]]
             min_temp = low_temp
     
     for item in result:
-        if(item[7] == ""):
+        if(item[heading["Max Humidity"]] == ""):
             continue
         
-        high_humid = int(item[7])
+        high_humid = int(item[heading["Max Humidity"]])
         
         if(max_humid < high_humid):
-            max_humid_date = item[0]
+            max_humid_date = item[heading["PKT"]]
             max_humid = high_humid
         
     return [str(max_temp), max_temp_date, str(min_temp), min_temp_date, str(max_humid), max_humid_date]
@@ -88,31 +95,25 @@ def given_month(month,year):
     for file in os.listdir(args.path):
         if (fnmatch.fnmatch(file, "*" + year + '_' + month + '*.txt')):month_files.append(file)
     
-    result=read_file(month_files)
+    result,heading = read_file(month_files)
     
     for item in result:
-        if(item[1] == ""):
+        if(item[heading["Max TemperatureC"]] == ""):
             continue
         
-        sum_max_temp = sum_max_temp + int(item[1])
+        sum_max_temp = sum_max_temp + int(item[heading["Max TemperatureC"]])
     
     for item in result:
-        if(item[1] == ""):
+        if(item[heading["Min TemperatureC"]] == ""):
             continue
         
-        sum_max_temp = sum_max_temp + int(item[1])
-    
-    for item in result:
-        if(item[3] == ""):
-            continue
-        
-        sum_min_temp = sum_min_temp + int(item[3])
+        sum_min_temp = sum_min_temp + int(item[heading["Min TemperatureC"]])
 
     for item in result:
-        if(item[8] == ""):
+        if(item[heading[" Mean Humidity"]] == ""):
             continue
         
-        sum_mean_humid = sum_mean_humid + int(item[8])
+        sum_mean_humid = sum_mean_humid + int(item[heading[" Mean Humidity"]])
     
     avg_max_temp = sum_max_temp / len(result)
     avg_min_temp = sum_min_temp / len(result)
@@ -130,62 +131,78 @@ def bar_chart_temp(month,year):
     return read_file(month_files)
 
 def bar_chart(month,year):
-    data = bar_chart_temp(month,year)
+    result, heading = bar_chart_temp(month,year)
     count = 0
-    
-    for i in data:
-        count = 0
-        print(i[0].split(sep = "-")[2],end = "")
-        
-        while not i[3] == "" and count < int(i[3]):
-            output = "\33[{}m".format(49) + "\33[{}m".format(31) + "+" + "\33[{}m".format(0)
-            print(output, end = "")
-            count += 1
 
-            
-        if (not i[1] == ""):
-            print(f"{i[1]}C")
-        else:
-            print("")
+    for i in result:
         count = 0
+        date = i[heading["PKT"]].split("-")[2]
+        min_temp = i[heading["Min TemperatureC"]]
+        max_temp = i[heading["Max TemperatureC"]]
         
-        print(i[0].split(sep = "-")[2],end = "")
-        while not i[3] == "" and count < int(i[3]):
-            output = "\33[{}m".format(49) + "\33[{}m".format(96) + "+" + "\33[{}m".format(0)
-            print(output, end = "")
-            count += 1
- 
-        if (not i[3] == ""):
-            print(f"{i[3]}C")
+        print(date, end="")
+
+        if min_temp != "":
+            min_temp = int(min_temp)
+            while count < min_temp:
+                output = "\33[49m\33[31m+\33[0m"
+                print(output, end="")
+                count += 1
+
+            print(f" {min_temp}C")
         else:
             print("")
+
+        count = 0
+        print(date, end="")
+
+        if max_temp != "":
+            max_temp = int(max_temp)
+            while count < max_temp:
+                output = "\33[49m\33[96m+\33[0m"
+                print(output, end="")
+                count += 1
+
+            print(f" {max_temp}C")
+        else:
+            print("")
+
+
 
 def bar_chart_task5(month,year):
-    data = bar_chart_temp(month,year)
+    result, heading = bar_chart_temp(month,year)
     count = 0
     
-    for i in data:
+    for i in result:
         count = 0
-        print(i[0].split(sep = "-")[2],end = "")
+        date = i[heading["PKT"]].split("-")[2]
+        min_temp = i[heading["Min TemperatureC"]]
+        max_temp = i[heading["Max TemperatureC"]]
         
-        while not i[3] == "" and count < int(i[3]):
-            output = "\33[{}m".format(49) + "\33[{}m".format(96) + "+" + "\33[{}m".format(0)
-            print(output, end = "")
+        print(date, end="")
+        
+        # Print bar chart for Min TemperatureC
+        while not min_temp == "" and count < int(min_temp):
+            output = "\33[49m\33[96m+\33[0m"
+            print(output, end="")
             count += 1
-    
-        count = 0        
-        while not i[3] == "" and count < int(i[3]):
-            output = "\33[{}m".format(49) + "\33[{}m".format(31) + "+" + "\33[{}m".format(0)
-            print(output, end = "")
+
+        count = 0
+        # Print bar chart for Max TemperatureC
+        while not max_temp == "" and count < int(max_temp):
+            output = "\33[49m\33[31m+\33[0m"
+            print(output, end="")
             count += 1
-         
-        if (not i[3] == ""):
-            print(f" {i[3]}",end = "C")
+        
+        # Print Max TempatureC
+        if not max_temp == "":
+            print(f" {max_temp}C", end="")
         else:
-            print("",end = "")
+            print("", end="")
             
-        if (not i[1] == ""):
-            print(f" - {i[1]}C")
+        # Print Min TempatureC
+        if not min_temp == "":
+            print(f" - {min_temp}C")
         else:
             print("")
 
@@ -194,18 +211,17 @@ def month_number_to_short_name(month_number):
 
 if(args.e):
     result = given_year(args.e)
-    print(result)
     
-    # abbr = month_number_to_short_name(int(result[1].split(sep = "-")[1]))
-    # date = result[1].split(sep = "-")[2]
-    # print("Task 1")
-    # print(f"Highest: {result[0]}C on {abbr} {date}")
-    # abbr = month_number_to_short_name(int(result[3].split(sep = "-")[1]))
-    # date = result[3].split(sep = "-")[2]
-    # print(f"Lowest: {result[2]}C on {abbr} {date}")
-    # abbr = month_number_to_short_name(int(result[5].split(sep = "-")[1]))
-    # date = result[5].split(sep = "-")[2]
-    # print(f"Highest: {result[4]}% on {abbr} {date}")
+    abbr = month_number_to_short_name(int(result[1].split(sep = "-")[1]))
+    date = result[1].split(sep = "-")[2]
+    print("Task 1")
+    print(f"Highest: {result[0]}C on {abbr} {date}")
+    abbr = month_number_to_short_name(int(result[3].split(sep = "-")[1]))
+    date = result[3].split(sep = "-")[2]
+    print(f"Lowest: {result[2]}C on {abbr} {date}")
+    abbr = month_number_to_short_name(int(result[5].split(sep = "-")[1]))
+    date = result[5].split(sep = "-")[2]
+    print(f"Highest: {result[4]}% on {abbr} {date}")
 
 if(args.a):
     month = args.a.split(sep = "/")[1]
