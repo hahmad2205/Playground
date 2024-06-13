b1617542f1d363d8fc732b9bf8a14af146205af6
hassaan <hassaan.ahmad@cogentlabs.co>
2024-06-13 12:53:12 +0500

    Dry principle and resolved and inilialization as inf at start of calculation fixed

--- weatherMan.py
+++ weatherMan.py
@@ -6,41 +6,59 @@ import os
 import re
 
 class WeatherReportGenerator:
-    
-    def filter_filename_by_year(self, user_input):
+        
+    def filter_filename_by_year_and_month(self, user_input, year = None, month_abbr = None):
+        if user_input.e:
+            pattern = f"*{user_input.e}*.txt"
+        else:
+            pattern = f"*{year}_{month_abbr}*.txt"
+            
         return [
             weather_file for weather_file in os.listdir(user_input.path)
-            if fnmatch.fnmatch(weather_file, f"*{user_input.e}*.txt")
+            if fnmatch.fnmatch(weather_file, pattern)
         ]
-
-    def parse_arguments_for_month(self, month):
+      
+    def parse_arguments_for_date(self, month):
         year_pattern = r'^\d{4}'
         month_pattern = r'(0?[1-9]|1[0-2])$'
         matched_year = re.search(year_pattern, month)
         matched_month_number = re.search(month_pattern, month)
         
-        if not matched_year or not matched_month_number:
-            return []
-        
-        year = matched_year.group()
-        month_abbr = calendar.month_abbr[int(matched_month_number.group())]
-        
-        return year, month_abbr
-    
-    def filter_filename_by_month(self, user_input, year, month_abbr):
-
-        for weather_file in os.listdir(user_input.path):
+        if matched_year and matched_month_number:    
+            year = matched_year.group()
+            month_abbr = calendar.month_abbr[int(matched_month_number.group())]
             
-            if fnmatch.fnmatch(weather_file, f"*{year}_{month_abbr}*.txt"):
-                return weather_file
-    
-    def read_file(self, file_name, user_input):
+            return year, month_abbr
         
-        with open(user_input.path + file_name, "r") as weather_file:
-            headings = weather_file.readline().strip().split(",")
-            weather_lines = weather_file.readlines()
-            weather_lines_record, date = self.covert_weather_data_in_required_format(headings, weather_lines)
-        return weather_lines_record, date
+        if matched_year:
+            year = matched_year.group()
+            return year
+        
+        return None
+    
+    def read_file(self, weather_file_names, user_input):
+        weather_lines_record = []
+        for weather_file_name in weather_file_names:
+            with open(user_input.path + weather_file_name, "r") as weather_file:
+                headings = weather_file.readline().strip().split(",")
+                weather_lines = weather_file.readlines()
+                weather_lines = self.covert_weather_data_in_required_format(headings, weather_lines)
+                weather_lines_record.extend(weather_lines)
+
+        return weather_lines_record
+    
+    def calculate_max_value(self, weather_record, key):
+        max_temperatures = []
+        for weather_line in weather_record:
+            
+            if weather_line.get(key):
+                current_value = int(weather_line.get(key))
+                max_temperatures.append(current_value)
+                max_value = max(max_temperatures)
+                date_abbr = weather_line["PKT"] if weather_line.get("PKT") else weather_line["PKST"]
+                max_value_date = date_abbr if current_value == int(max_value) else max_value_date
+        
+        return max_value, max_value_date
     
     def calculate_average(self, weather_record, key):
         sum_of_values = 0
@@ -56,86 +74,59 @@ class WeatherReportGenerator:
         return int(sum_of_values / len(weather_record))
     
     def covert_weather_data_in_required_format(self, headings, weather_lines):
-        date_abbr = "PKT" if "PKT" in headings else "PKST"
         weather_lines_record = [{headings[index] : item for index, item in enumerate(line.strip().split(sep = ","))} for line in weather_lines]
-        return weather_lines_record, date_abbr
-    
-    def generate_weather_record_for_month(self,user_input, key):
-        year, month_abbr = self.parse_arguments_for_month(key)
-        month_files = self.filter_filename_by_month(user_input, year, month_abbr)
-        return self.read_file(month_files, user_input)
-               
-    def calculate_max_temperature_year(self, weather_record, date_abbr, max_temperature, max_temperature_date):
-        for weather_line in weather_record:
-            max_temperature_value = weather_line.get("Max TemperatureC")
-            
-            if max_temperature_value:
-                current_temperature = int(max_temperature_value)
-                max_temperature = max(max_temperature, current_temperature)
-                max_temperature_date = weather_line[date_abbr] if current_temperature == int(max_temperature) else max_temperature_date
-        return max_temperature, max_temperature_date
+        return weather_lines_record
+  
+    def generate_weather_record_for_month(self, valid_date, user_input):
+        year, month_abbr = valid_date
+        month_files = self.filter_filename_by_year_and_month(user_input, year, month_abbr)
+        weather_record = self.read_file(month_files, user_input)
         
-    def calculate_min_temperature_year(self, weather_record, date_abbr, min_temperature, min_temperature_date):
+        return weather_record
+          
+    def calculate_min_temperature_year(self, weather_record):
+        min_temperatures = []
         for weather_line in weather_record:
             min_temperature_value = weather_line.get("Min TemperatureC")
             
             if min_temperature_value:
                 current_temperature = int(min_temperature_value)
-                min_temperature = min(current_temperature, min_temperature)
-                min_temperature_date = weather_line[date_abbr] if current_temperature == int(min_temperature) else min_temperature_date
+                min_temperatures.append(current_temperature)
+                min_temperature = min(min_temperatures)
+                date_abbr = weather_line["PKT"] if weather_line.get("PKT") else weather_line["PKST"]
+                min_temperature_date = date_abbr if current_temperature == int(min_temperature) else min_temperature_date
+        
         return min_temperature, min_temperature_date
         
-    def calculate_max_humid_year(self, weather_record, date_abbr, max_humid, max_humid_date):
-        for weather_line in weather_record:
-            max_humidity_value = weather_line.get("Max Humidity")
-            
-            if max_humidity_value:
-                current_humid = int(max_humidity_value)
-                max_humid = max(current_humid, max_humid)
-                max_humid_date = weather_line[date_abbr] if current_humid == int(max_humid) else max_humid_date
-        return max_humid, max_humid_date
-
-    def calculate_avg_max_temperature_month(self, weather_record):
-        return self.calculate_average(weather_record, "Max TemperatureC")
-    
-    def calculate_avg_min_temperature_month(self,weather_record):        
-        return self.calculate_average(weather_record, "Min TemperatureC")
-        
-    def calculate_avg_mean_humid_month(self, weather_record):
-        return self.calculate_average(weather_record, " Mean Humidity")
-    
     def generate_year_weather_report(self, user_input):
-        max_temperature = float("-inf")
-        min_temperature = float("inf")
-        max_humid = float("-inf")
-        max_temperature_date = None
-        min_temperature_date = None
-        max_humid_date = None
-        year_files = self.filter_filename_by_year(user_input)
-        
-        for file_name in year_files:
-            weather_record, date_abbr = self.read_file(file_name, user_input)
+        year_files = self.filter_filename_by_year_and_month(user_input)
+        weather_record = self.read_file(year_files, user_input)
+
+        if len(weather_record):
+            max_temperature, max_temperature_date = self.calculate_max_value(weather_record, "Max TemperatureC")
+            min_temperature, min_temperature_date = self.calculate_max_value(weather_record,"Max Humidity")
+            max_humid, max_humid_date = self.calculate_max_humid_year(weather_record)
+            self.print_year_weather_report(max_temperature, max_temperature_date, min_temperature, min_temperature_date, max_humid, max_humid_date)
+            return
             
-            max_temperature, max_temperature_date = self.calculate_max_temperature_year(weather_record, date_abbr, max_temperature, max_temperature_date)
-            min_temperature, min_temperature_date = self.calculate_min_temperature_year(weather_record, date_abbr, min_temperature, min_temperature_date)
-            max_humid, max_humid_date = self.calculate_max_humid_year(weather_record, date_abbr, max_humid, max_humid_date)
-            
-        self.print_year_weather_report(max_temperature, max_temperature_date, min_temperature, min_temperature_date, max_humid, max_humid_date)
+    def generate_month_weather_report(self, user_input, valid_date):
+        weather_record = self.generate_weather_record_for_month(valid_date, user_input)
         
-    def generate_month_weather_report(self, user_input):
-        weather_record, _ = self.generate_weather_record_for_month(user_input, user_input.a)
-        avg_high_temperature = self.calculate_avg_max_temperature_month(weather_record)
-        avg_low_temperature = self.calculate_avg_min_temperature_month(weather_record)
-        avg_mean_humid = self.calculate_avg_mean_humid_month(weather_record)
-        self.print_month_weather_report(avg_high_temperature, avg_low_temperature, avg_mean_humid)
+        if len(weather_record):
+            avg_high_temperature = self.calculate_average(weather_record, "Max TemperatureC")
+            avg_low_temperature = self.calculate_average(weather_record, "Min TemperatureC")
+            avg_mean_humid = self.calculate_average(weather_record, " Mean Humidity")
+            self.print_month_weather_report(avg_high_temperature, avg_low_temperature, avg_mean_humid)
         
-    def generate_month_temperature_bar_chart(self, user_input):
-        weather_record, date_abbr = self.generate_weather_record_for_month(user_input, user_input.c)    
-        self.print_month_temperature_bar_chart(weather_record, date_abbr)
+    def generate_month_temperature_bar_chart(self, user_input, valid_date):
+        weather_record = self.generate_weather_record_for_month(valid_date, user_input)
+        if len(weather_record):
+            self.print_month_temperature_bar_chart(weather_record)
     
-    def generate_month_temperature_bar_chart_bonus_task(self, user_input):
-        weather_record, date_abbr = self.generate_weather_record_for_month(user_input, user_input.b)
-        self.print_month_temperature_bar_chart_bonus_task(weather_record, date_abbr)
+    def generate_month_temperature_bar_chart_bonus_task(self, user_input, valid_date):
+        weather_record = self.generate_weather_record_for_month(valid_date, user_input)
+        if len(weather_record):
+            self.print_month_temperature_bar_chart_bonus_task(weather_record)
     
     def print_star(self, plus_count, color):
         
@@ -148,10 +139,11 @@ class WeatherReportGenerator:
         print(f"Lowest Average: {avg_low_temperature}C")
         print(f"Average Mean Humidity: {avg_mean_humid}C", end = "\n\n")
     
-    def print_month_temperature_bar_chart(self, weather_record, date_abbr):
+    def print_month_temperature_bar_chart(self, weather_record):
         
         for line in weather_record:
-            date = datetime.datetime.strptime(line[date_abbr], "%Y-%m-%d")
+            date_abbr = line["PKT"] if line.get("PKT") else line["PKST"]
+            date = datetime.datetime.strptime(date_abbr, "%Y-%m-%d")
             date = date.day
             
             if line["Max TemperatureC"]:
@@ -164,12 +156,13 @@ class WeatherReportGenerator:
                 self.print_star(int(line["Min TemperatureC"]), "\033[94m")
                 print(f"{line['Min TemperatureC']}C")
     
-    def print_month_temperature_bar_chart_bonus_task(self, weather_record, date_abbr):
+    def print_month_temperature_bar_chart_bonus_task(self, weather_record):
         
         for line in weather_record:
             
             if line["Min TemperatureC"] or line["Max TemperatureC"]:
-                date = datetime.datetime.strptime(line[date_abbr], "%Y-%m-%d")
+                date_abbr = line["PKT"] if line.get("PKT") else line["PKST"]
+                date = datetime.datetime.strptime(date_abbr, "%Y-%m-%d")
                 date = date.day
                 print(date, end = "    ")
                 
@@ -209,15 +202,33 @@ def main():
     user_input = parser.parse_args()
     
     if user_input.e:
-        weather_report_generator.generate_year_weather_report(user_input)
+        if weather_report_generator.parse_arguments_for_date(user_input.e):
+            weather_report_generator.generate_year_weather_report(user_input)
+        else:
+            print("Error: Not a valid year")
     
     if user_input.a:
-        weather_report_generator.generate_month_weather_report(user_input)
+        valid_date = weather_report_generator.parse_arguments_for_date(user_input.a)
+        
+        if valid_date:
+            weather_report_generator.generate_month_weather_report(user_input, valid_date)
+        else:
+            print("Error: Not a valid year")
         
     if user_input.c:
-        weather_report_generator.generate_month_temperature_bar_chart(user_input)
+        valid_date = weather_report_generator.parse_arguments_for_date(user_input.c)
+        
+        if valid_date:
+            weather_report_generator.generate_month_temperature_bar_chart(user_input, valid_date)
+        else:
+            print("Error: Not a valid year")
     
     if user_input.b:
-        weather_report_generator.generate_month_temperature_bar_chart_bonus_task(user_input)
+        valid_date = weather_report_generator.parse_arguments_for_date(user_input.b)
+        
+        if valid_date:
+            weather_report_generator.generate_month_temperature_bar_chart_bonus_task(user_input, valid_date)
+        else:
+            print("Error: Not a valid year")
     
 main()
