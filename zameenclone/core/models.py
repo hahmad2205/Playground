5cc0b9835552562d0d3c1c44b63c9a7160e6a477
hassaan <hassaan.ahmad@cogentlabs.co>
2024-07-03 12:14:00 +0500

    Naming conventions and indenation issues resolved

--- zameenclone/core/models.py
+++ zameenclone/core/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
 
 
-class Amenity(models.Model):
-    amenity_name = models.CharField(max_length=50)
-
+class Amenity(TimeStampedModel):
+    name = models.CharField(max_length=50)
     
-class AmenityOption(models.Model):
-    amenity_type = models.ForeignKey(Amenity, on_delete=models.CASCADE, related_name="amenities")
-    amenity_key = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+
+class AmenityOption(TimeStampedModel):
+    key = models.CharField(max_length=50)
+    
+    type = models.ForeignKey(Amenity, on_delete=models.CASCADE, related_name="options")
+    
+    def __str__(self):
+        return self.key
 
--- zameenclone/properties/models.py
+++ zameenclone/properties/models.py
@@ -1,42 +1,52 @@
 from django.db import models
-
-from core.models import AmenityOption
+from core.models import AmenityOption, TimeStampedModel
 from users.models import User
 
 
-# Create your models here.
 
-
-class Property(models.Model):
-    property_title = models.CharField(max_length=100)
-    property_header = models.CharField(max_length=100)
-    property_type = models.CharField(max_length=50)
-    property_price = models.PositiveIntegerField()
-    property_location = models.CharField(max_length=50)
+class Property(TimeStampedModel):
+    area = models.CharField(max_length=255)
+    description_text = models.TextField()
+    header = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    listing_purpose = models.CharField(max_length=255)
     number_of_bath = models.PositiveIntegerField()
     number_of_bed = models.PositiveIntegerField()
-    property_area = models.CharField(max_length=50)
-    listing_purpose = models.CharField(max_length=50)
-    description_text = models.TextField()
+    price = models.PositiveIntegerField()
+    title = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
     whatsapp = models.CharField(max_length=13)
     
     def __str__(self):
-        return self.property_title
+        return self.title
 
 
-class PropertyImages(models.Model):
+class PropertyImages(TimeStampedModel):
     image_link = models.TextField()
-    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name="property_images")
-
-
-class PropertyOffers(models.Model):
-    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name="property_offers")
-    offer_price = models.PositiveIntegerField()
-    offered_to = models.ForeignKey(Property, on_delete=models.CASCADE, related_name="property_offers_to")
-    offered_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="property_offers_from")
-
     
-class PropertyAmenity(models.Model):
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name="property_images")
+    
+    def __str__(self):
+        return self.property.title
+
+
+class PropertyOffers(TimeStampedModel):
+    offer_price = models.PositiveIntegerField()
+    
+    offered_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name="offers_received")
+    offered_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="offers_made")
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name="property_offers")
+    
+    def __str__(self):
+        return f"{self.property.title} - {self.offer_price}"
+
+
+class PropertyAmenity(TimeStampedModel):
+    value = models.PositiveIntegerField()
+    
+    key = models.ForeignKey(AmenityOption, on_delete=models.CASCADE, related_name='amenity_options')
     property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='property_amenities')
-    amenity_option = models.ForeignKey(AmenityOption, on_delete=models.CASCADE, related_name='property_amenities')
-    property_amenity_key = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.property.title} - {self.key.key}: {self.value}"
+
