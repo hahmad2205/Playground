051cd6a49ca1a39acc555631efd673321aa4c0d7
hassaan <hassaan.ahmad@cogentlabs.co>
2024-07-04 11:26:22 +0500

    Login logout view implemented

--- zameenclone/users/views.py
+++ zameenclone/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def login_user(request):
+    if request.method == "POST":
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return redirect('logout')
+        else:
+            return render(request, "users/login.html", {"message": "Invalid username or password"})
+    else:
+        return render(request, "users/login.html", {})
+
+def logout_user(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('login')
+    else:
+        return render(request, "users/logout.html", {})
