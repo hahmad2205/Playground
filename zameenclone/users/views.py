bbc7591a78ef14487e7203a083b8a33ea09051df
hassaan <hassaan.ahmad@cogentlabs.co>
2024-07-04 11:50:58 +0500

    User will redirect to logout url if already login

--- zameenclone/users/views.py
+++ zameenclone/users/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 
 def login_user(request):
-    if request.method == "POST":
+    if request.user.is_authenticated:
+        response = redirect('logout')
+    elif request.method == "POST":
         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is not None:
             login(request, user)
