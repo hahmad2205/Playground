f1a5483523ce27ad48f1f7dcc1abe64d0e0d0681
hassaan <hassaan.ahmad@cogentlabs.co>
2024-07-04 12:43:37 +0500

    Admin file updated

--- zameenclone/properties/admin.py
+++ zameenclone/properties/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Property, PropertyImages
 
-# Register your models here.
+class ImageInline(admin.StackedInline):
+    model = PropertyImages
+    extra = 5
+
+class PropertyAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
+admin.site.register(Property)
--- zameenclone/users/views.py
+++ zameenclone/users/views.py
@@ -3,12 +3,12 @@ from django.contrib.auth import authenticate, login, logout
 
 def login_user(request):
     if request.user.is_authenticated:
-        response = redirect('logout')
+        response = redirect('property/')
     elif request.method == "POST":
         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is not None:
             login(request, user)
-            response = redirect('logout')
+            response = redirect('properties/')
         else:
             response = render(request, "users/login.html", {"message": "Invalid username or password"})
     else:
@@ -21,6 +21,6 @@ def logout_user(request):
         logout(request)
         response = redirect('login')
     else:
-        response = render(request, "users/logout.html", {})
+        response = redirect('property/')
     
     return response
