3e87936ba24be28361273d68b208f9095f09a571
hassaan <hassaan.ahmad@cogentlabs.co>
2024-07-07 03:36:51 +0500

    Pagination for properties implemented

--- zameenclone/properties/views.py
+++ zameenclone/properties/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import Property
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+
+from .models import Property
 
 def get_images_from_property(properties):
     properties_with_images = []
@@ -13,15 +15,29 @@ def get_images_from_property(properties):
         
     return properties_with_images
 
+def create_pagination_of_properties(properties, request):
+    properties_with_images = get_images_from_property(properties)
+    paginator = Paginator(properties_with_images, 25)
+    page_number = request.GET.get("page")
+    return paginator.get_page(page_number)
+
 def marketplace_listings(request):    
-    properties = Property.objects.all()
-    context = {"properties": get_images_from_property(properties), "path": request.path}
-    
-    return render(request, "properties/property_listing.html", context)
+    properties = Property.objects.all()    
+    return render(
+        request, "properties/property_listing.html",
+        {
+            "properties": create_pagination_of_properties(properties, request),
+            "path": request.path
+        }
+    )
 
 @login_required
 def my_listings(request):
     properties = Property.objects.filter(owner=request.user)
-    context = {"properties": get_images_from_property(properties)}
-
-    return render(request, "properties/property_listing.html", context)
+    return render(
+        request, "properties/property_listing.html",
+        {
+            "properties": create_pagination_of_properties(properties, request),
+            "path": request.path
+        }
+    )
