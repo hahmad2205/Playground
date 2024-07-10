2e309309cecf960024fc69bb753def1915fb03ca
hassaan <hassaan.ahmad@cogentlabs.co>
2024-07-10 11:57:10 +0500

    Deleting property functionality added

--- zameenclone/properties/views.py
+++ zameenclone/properties/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 
-from .models import Property
+from .models import Property, PropertyImages, PropertyAmenity
 from core.utils import create_pagination
 
 @login_required
@@ -23,9 +23,19 @@ def marketplace(request):
 @login_required
 def properties(request):
     if request.method == "POST":
-        properties = Property.objects.filter(Q(title__contains=request.POST.get("search")) | Q(location__contains=request.POST.get("search")))
+        if request.POST.get("search"):
+            properties = Property.objects.filter(Q(title__contains=request.POST.get("search")) | Q(location__contains=request.POST.get("search")))
+        elif request.POST.get("property_id"):
+            property_instance = Property.objects.get(pk=request.POST["property_id"])
+            if property_instance:
+                PropertyImages.objects.filter(property=property_instance).delete()
+                PropertyAmenity.objects.filter(property=property_instance).delete()
+                property_instance.delete()
+                properties = Property.objects.filter(owner=request.user)
+
     else:
         properties = Property.objects.filter(owner=request.user)
+    
     
     return render(
         request, "properties/property_listing.html",
