331a378948c780ce545f7178d5f1200c1a8b8937
hassaan <hassaan.ahmad@cogentlabs.co>
2024-07-15 23:24:53 +0500

    Seach on title and location implemented

--- zameenclone/properties/views/api_views.py
+++ zameenclone/properties/views/api_views.py
@@ -1,5 +1,6 @@
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Q
 
 from ..models import Property
 from ..serializers import PropertySerializer
@@ -7,9 +8,16 @@ from ..serializers import PropertySerializer
 
 class PropertyMarketplaceListAPIView(APIView):
     def get(self, request):
-        properties = (
+        query = request.GET.get("search")
+        queryset = (
             Property.objects.filter(is_active=True, is_sold=False).
             prefetch_related("images", "amenities", "offers", "owner")
+        )
+        properties = (
+            queryset.filter(
+                Q(title__icontains=query) | Q(location__icontains=query)
+            )
+            if query else queryset
         )
         serializer = PropertySerializer(properties, many=True)
         return Response(data=serializer.data)
@@ -17,9 +25,16 @@ class PropertyMarketplaceListAPIView(APIView):
 
 class PropertyListAPIView(APIView):
     def get(self, request):
+        query = request.GET.get("search")
+        queryset = (
+            Property.objects.filter(is_active=True, is_sold=False, owner=request.user).
+            prefetch_related("images", "amenities", "offers", "owner")
+        )
         properties = (
-            Property.objects.filter(owner=request.user, is_active=True, is_sold=False).
-            select_related("owner").prefetch_related("images", "amenities", "offers")
+            queryset.filter(
+                Q(title__icontains=query) | Q(location__icontains=query)
+            )
+            if query else queryset
         )
         serializer = PropertySerializer(properties, many=True)
         return Response(data=serializer.data)
