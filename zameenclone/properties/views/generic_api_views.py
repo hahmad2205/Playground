362042db7ddbb27693f4934ccf0ae894a02c2172
hassaan <hassaan.ahmad@cogentlabs.co>
2024-07-25 11:13:23 +0500

    Not used imports removed

--- zameenclone/properties/views/generic_api_views.py
+++ zameenclone/properties/views/generic_api_views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import get_object_or_404, get_list_or_404
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
-from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
