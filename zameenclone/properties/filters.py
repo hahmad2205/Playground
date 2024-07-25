d02cb6e4b318d12c575b02e1e02133b99a9c0ed1
hassaan <hassaan.ahmad@cogentlabs.co>
2024-07-25 11:20:31 +0500

    Django default filter used

--- zameenclone/properties/filters.py
+++ zameenclone/properties/filters.py
@@ -2,24 +2,14 @@ import django_filters
 
 from properties.models import Property
 
+
 class PropertyFilter(django_filters.FilterSet):
-    price = django_filters.NumberFilter()
-    price__gt = django_filters.NumberFilter(field_name="price", lookup_expr="gt")
-    price__lt = django_filters.NumberFilter(field_name="price", lookup_expr="lt")
-
-    number_of_bed = django_filters.NumberFilter()
-    number_of_bed__gt = django_filters.NumberFilter(field_name="number_of_bed", lookup_expr="gt")
-    number_of_bed__lt = django_filters.NumberFilter(field_name="number_of_bed", lookup_expr="lt")
-    
-    number_of_bath = django_filters.NumberFilter()
-    number_of_bath__gt = django_filters.NumberFilter(field_name="number_of_bath", lookup_expr="gt")
-    number_of_bath__lt = django_filters.NumberFilter(field_name="number_of_bath", lookup_expr="lt")
-    
-    area = django_filters.NumberFilter()
-    area__gt = django_filters.NumberFilter(field_name="area", lookup_expr="gt")
-    area__lt = django_filters.NumberFilter(field_name="area", lookup_expr="lt")
-
     class Meta:
         model = Property
-        fields = ["price", "number_of_bed", "number_of_bath", "area"]
+        fields = {
+            "price": ["exact", "gt", "lt"],
+            "area": ["exact", "gt", "lt"],
+            "number_of_bed": ["exact", "gt", "lt"],
+            "number_of_bath": ["exact", "gt", "lt"]
+        }
 
