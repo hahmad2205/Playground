1eebc013ff4d90281a14a10abc558c2966991ba9
hassaan <hassaan.ahmad@cogentlabs.co>
2024-07-25 16:25:33 +0500

    Commented code removed

--- zameenclone/properties/serializers.py
+++ zameenclone/properties/serializers.py
@@ -84,12 +84,6 @@ class PropertyAmenitySerializer(serializers.ModelSerializer):
         fields = ["id", "amenity", "value", "property"]
         read_only_fields = ["property", "amenity"]
 
-    # def create(self, validated_data):
-    #     amenity_data = validated_data.pop("amenity")
-    #     amenity = get_object_or_404(AmenityOption, pk=amenity_data.get("option"))
-    #     validated_data["amenity"] = amenity
-    #     return super().create(validated_data)
-
 
 class PropertyListDetailSerializer(serializers.ModelSerializer):
     images = PropertyImageSerializer(many=True)
@@ -130,7 +124,7 @@ class PropertySerializer(serializers.ModelSerializer):
             save_images(images, property)
             save_amenities(amenities, property)
 
-            return property
+        return property
 
 
 class PropertyImagesAmenitiesUpdateSerializer(serializers.Serializer):
