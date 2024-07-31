6360a61ad21a86432b464e530fb133741d42e932
hassaan <hassaan.ahmad@cogentlabs.co>
2024-07-31 12:22:21 +0500

    Generic viewsets updated to model viewsets.

--- zameenclone/core/views/view_sets.py
+++ zameenclone/core/views/view_sets.py
@@ -1,29 +1,20 @@
 from django.shortcuts import get_list_or_404, get_object_or_404
-from rest_framework.viewsets import GenericViewSet
-from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-from core.models import Amenity, AmenityOption
+from core.models import Amenity
 from core.serializers import AmenitySerializer, AmenityOptionListSerializer
 
 
-class AmenityListAPIView(GenericViewSet):
+class AmenityListAPIView(ModelViewSet):
     serializer_class = AmenitySerializer
-
-    def list(self, request):
-        queryset = get_list_or_404(Amenity, is_active=True)
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+    queryset = get_list_or_404(Amenity, is_active=True)
 
 
-class AmenityOptionListAPIView(GenericViewSet):
+class AmenityOptionListAPIView(ModelViewSet):
     serializer_class = AmenityOptionListSerializer
     pagination_class = None
 
     def get_queryset(self):
-        amenity = get_object_or_404(Amenity, pk=self.kwargs.get('pk'))
+        amenity = get_object_or_404(Amenity, pk=self.kwargs.get("pk"))
         return amenity.options.filter(is_active=True)
 
-    def list(self, request, pk=None):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
